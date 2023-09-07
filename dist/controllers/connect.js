5895014d3503def4a99b7a1e679dc232cc045db0
Swapnil Wakchaure <swapnilwakchaure2411999@gmail.com>
2023-09-07 12:06:38 +0530

    update readme

--- dist/controllers/connect.js
+++ dist/controllers/connect.js
@@ -3,6 +3,6 @@ import mongoose from 'mongoose';
 dotenv.config();
 // const mongoURL: string = String(process.env.mongoURL);
 const mongoURL = 'mongodb://swapnilwakchaure:mongodb@ac-hozldxs-shard-00-00.pgstk4w.mongodb.net:27017,ac-hozldxs-shard-00-01.pgstk4w.mongodb.net:27017,ac-hozldxs-shard-00-02.pgstk4w.mongodb.net:27017/eventapp?ssl=true&replicaSet=atlas-rk5qgc-shard-0&authSource=admin&retryWrites=true&w=majority';
-console.log('mongoURL: ', mongoURL);
+// console.log('mongoURL: ',mongoURL);
 const connection = mongoose.connect(mongoURL);
 export { connection };
--- src/controllers/connect.ts
+++ src/controllers/connect.ts
@@ -7,7 +7,7 @@ dotenv.config();
 
 const mongoURL = 'mongodb://swapnilwakchaure:mongodb@ac-hozldxs-shard-00-00.pgstk4w.mongodb.net:27017,ac-hozldxs-shard-00-01.pgstk4w.mongodb.net:27017,ac-hozldxs-shard-00-02.pgstk4w.mongodb.net:27017/eventapp?ssl=true&replicaSet=atlas-rk5qgc-shard-0&authSource=admin&retryWrites=true&w=majority'
 
-console.log('mongoURL: ',mongoURL);
+// console.log('mongoURL: ',mongoURL);
 const connection = mongoose.connect(mongoURL);
 
 
